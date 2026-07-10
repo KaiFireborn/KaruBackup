4f1ec309436da6c6af0f00e439fc5a83a4b6b534
KaiFireborn <blitzmasterev3@gmail.com>
2026-07-10 14:07:52 +0200

    feat: requirements.txt and auto install addedw

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "Initial setup done" >> initial_setup_marker.kf
+echo "Assuming python installed. Installing dependencies via pip "
+pip install -r ./requirements.txt
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OVERDUE_JOBS_SCRIPT="$SCRIPT_DIR/run_overdue_backups.py"
 
@@ -9,8 +11,11 @@ if [ -f "$OVERDUE_JOBS_SCRIPT" ]; then
     # add to crontab only if the script call is not yet there
     CRON_JOB="*/10 * * * * /usr/bin/python3 $OVERDUE_JOBS_SCRIPT"
     (crontab -l 2>/dev/null | grep -Fq "$OVERDUE_JOBS_SCRIPT") || (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
-    
+    echo "Added cron job for $OVERDUE_JOBS_SCRIPT if not there yet (you can check with \`crontab -e\`)"    
 else
     echo "Error: script to run not found at $OVERDUE_JOBS_SCRIPT" >&2
     exit 1
 fi
+
+echo "Initial setup done" >> initial_setup_marker.kf
+
